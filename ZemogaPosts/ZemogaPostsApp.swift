aafb532ff05fb69487be8982d4e418693d639951
Sergio Bravo <sergio.bravo@monstar-lab.com>
2022-07-12 11:16:46 -0500

    Initial Commit

---
+++ ZemogaPosts/ZemogaPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZemogaPostsApp.swift
+//  ZemogaPosts
+//
+//  Created by Sergio Bravo on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ZemogaPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
